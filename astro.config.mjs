a0900455b90840520f6c2f6aab3788d0b55d4755
Michele Volonghi <michele.volonghi@gmail.com>
2026-01-19 18:59:49 +0000

    Remove sharp (bundled in Astro), simplify image config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://borgoguglielmo.pages.dev',
 	image: {
-		service: {
-			entrypoint: 'astro/assets/services/sharp',
-			config: {
-				limitInputPixels: false,
-			},
-		},
 		domains: [],
 		remotePatterns: [],
-		responsiveStyles: true,
-		layout: 'full-width',
-		objectFit: 'cover',
-		objectPosition: 'center',
-		breakpoints: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
 	},
 	vite: {
 		css: {
