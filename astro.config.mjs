89cafcc038d6f63f23f4196355a50b7130dfef73
Michele Volonghi <michele.volonghi@gmail.com>
2026-01-19 19:11:07 +0000

    Add @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://borgoguglielmo.pages.dev',
+	integrations: [sitemap()],
 	image: {
 		layout: 'constrained',
 		responsiveStyles: true,
